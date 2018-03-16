3297fdd9a00c51e73cadaaab02d994deb2582d8e
imzf <you@example.com>
2018-03-16 22:58:32 +0800

    pure_v11.7

--- crypto/lz4.c
+++ crypto/lz4.c
@@ -64,13 +64,16 @@ static int lz4_compress_crypto(struct crypto_tfm *tfm, const u8 *src,
 static int lz4_decompress_crypto(struct crypto_tfm *tfm, const u8 *src,
 			      unsigned int slen, u8 *dst, unsigned int *dlen)
 {
-	int out_len = LZ4_decompress_safe(src, dst, slen, *dlen);
+	int err;
+	size_t tmp_len = *dlen;
+	size_t __slen = slen;
 
-	if (out_len < 0)
-		return out_len;
+	err = lz4_decompress_unknownoutputsize(src, __slen, dst, &tmp_len);
+	if (err < 0)
+		return -EINVAL;
 
-	*dlen = out_len;
-	return 0;
+	*dlen = tmp_len;
+	return err;
 }
 
 static struct crypto_alg alg_lz4 = {
